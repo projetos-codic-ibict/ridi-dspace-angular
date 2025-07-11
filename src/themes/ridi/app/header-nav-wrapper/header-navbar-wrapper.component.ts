7b9919371eeef09956c3d2eccb58241d0d44c994
Jesiel Viana <jesielviana@proton.me>
2025-07-11 18:01:04 -0300

    alterando header

--- src/themes/ridi/app/header-nav-wrapper/header-navbar-wrapper.component.ts
+++ src/themes/ridi/app/header-nav-wrapper/header-navbar-wrapper.component.ts
@@ -1,26 +1,27 @@
-import {
-  AsyncPipe,
-  NgClass,
-} from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { ThemedHeaderComponent } from '../../../../app/header/themed-header.component';
 import { HeaderNavbarWrapperComponent as BaseComponent } from '../../../../app/header-nav-wrapper/header-navbar-wrapper.component';
 import { ThemedNavbarComponent } from '../../../../app/navbar/themed-navbar.component';
+import { slideMobileNav } from '../../../../app/shared/animations/slide';
 
+/**
+ * This component represents a wrapper for the horizontal navbar and the header
+ */
 @Component({
   selector: 'ds-themed-header-navbar-wrapper',
-  // styleUrls: ['./header-navbar-wrapper.component.scss'],
-  styleUrls: ['../../../../app/header-nav-wrapper/header-navbar-wrapper.component.scss'],
-  // templateUrl: './header-navbar-wrapper.component.html',
-  templateUrl: '../../../../app/header-nav-wrapper/header-navbar-wrapper.component.html',
+  styleUrls: ['header-navbar-wrapper.component.scss'],
+  templateUrl: 'header-navbar-wrapper.component.html',
   standalone: true,
   imports: [
     AsyncPipe,
-    NgClass,
     ThemedHeaderComponent,
     ThemedNavbarComponent,
+    TranslateModule,
   ],
+  animations: [slideMobileNav],
 })
 export class HeaderNavbarWrapperComponent extends BaseComponent {
 }
--- src/themes/ridi/app/header/header.component.ts
+++ src/themes/ridi/app/header/header.component.ts
@@ -1,22 +1,28 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
 import { ThemedLangSwitchComponent } from 'src/app/shared/lang-switch/themed-lang-switch.component';
 
 import { ContextHelpToggleComponent } from '../../../../app/header/context-help-toggle/context-help-toggle.component';
 import { HeaderComponent as BaseComponent } from '../../../../app/header/header.component';
+import { ThemedNavbarComponent } from '../../../../app/navbar/themed-navbar.component';
 import { ThemedSearchNavbarComponent } from '../../../../app/search-navbar/themed-search-navbar.component';
 import { ThemedAuthNavMenuComponent } from '../../../../app/shared/auth-nav-menu/themed-auth-nav-menu.component';
 import { ImpersonateNavbarComponent } from '../../../../app/shared/impersonate-navbar/impersonate-navbar.component';
 
+/**
+ * Represents the header with the logo and simple navigation
+ */
 @Component({
   selector: 'ds-themed-header',
-  // styleUrls: ['header.component.scss'],
-  styleUrls: ['../../../../app/header/header.component.scss'],
-  // templateUrl: 'header.component.html',
-  templateUrl: '../../../../app/header/header.component.html',
+  styleUrls: ['header.component.scss'],
+  templateUrl: 'header.component.html',
   standalone: true,
   imports: [
     AsyncPipe,
@@ -26,9 +32,16 @@ import { ImpersonateNavbarComponent } from '../../../../app/shared/impersonate-n
     RouterLink,
     ThemedAuthNavMenuComponent,
     ThemedLangSwitchComponent,
+    ThemedNavbarComponent,
     ThemedSearchNavbarComponent,
     TranslateModule,
   ],
 })
-export class HeaderComponent extends BaseComponent {
+export class HeaderComponent extends BaseComponent implements OnInit {
+  public isNavBarCollapsed$: Observable<boolean>;
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.isNavBarCollapsed$ = this.menuService.isMenuCollapsed(this.menuID);
+  }
 }
