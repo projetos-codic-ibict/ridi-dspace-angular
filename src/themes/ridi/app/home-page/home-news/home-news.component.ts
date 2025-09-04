147801f1c494d31e22277e339fb6b590cc28efa9
Gabriel Marcos <gabrielmarcosbguimaraes@gmail.com>
2025-09-04 08:49:15 -0300

    adicionando bg do ridi e modificando cores

--- src/themes/ridi/app/home-page/home-news/home-news.component.ts
+++ src/themes/ridi/app/home-page/home-news/home-news.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { HomeNewsComponent as BaseComponent } from '../../../../../app/home-page/home-news/home-news.component';
+import { ThemedSearchFormComponent } from 'src/app/shared/search-form/themed-search-form.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'ds-themed-home-news',
   templateUrl: './home-news.component.html',
   styleUrls: ['./home-news.component.scss'],
   standalone: true,
+  imports: [ThemedSearchFormComponent, TranslateModule],
 })
 export class HomeNewsComponent extends BaseComponent {}
 
