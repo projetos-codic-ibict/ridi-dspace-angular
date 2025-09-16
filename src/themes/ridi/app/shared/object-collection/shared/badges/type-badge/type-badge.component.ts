c6de5d908fa96afd29a747e4901d10b4ae49c4a5
Francisco <francisco.carvalho.q@gmail.com>
2025-09-16 01:50:16 -0300

    adicionando badge de dc.type nos itens

--- src/themes/ridi/app/shared/object-collection/shared/badges/type-badge/type-badge.component.ts
+++ src/themes/ridi/app/shared/object-collection/shared/badges/type-badge/type-badge.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { TypeBadgeComponent as BaseComponent } from 'src/app/shared/object-collection/shared/badges/type-badge/type-badge.component';
+import { DSpaceObject } from '../../../../../../../../app/core/shared/dspace-object.model';
+import { hasValue, isEmpty } from '../../../../../../../../app/shared/empty.util';
+import { getResourceTypeValueFor } from '../../../../../../../../app/core/cache/object-cache.reducer';
 
 @Component({
   selector: 'ds-themed-type-badge',
@@ -12,5 +14,47 @@ import { TypeBadgeComponent as BaseComponent } from 'src/app/shared/object-colle
     TranslateModule,
   ],
 })
-export class TypeBadgeComponent extends BaseComponent {
+export class TypeBadgeComponent {
+  private _object: DSpaceObject;
+  private _typeMessage: string;
+
+  /**
+   * The component used to retrieve the type from
+   */
+  @Input() set object(object: DSpaceObject) {
+    this._object = object;
+
+    const renderTypes = this._object.getRenderTypes();
+    if (!isEmpty(renderTypes.length)) {
+      const renderType = renderTypes[0];
+      if (renderType instanceof Function) {
+        const resourceTypeValue = getResourceTypeValueFor(object.type);
+        if (hasValue(resourceTypeValue)) {
+          this._typeMessage = this.getTypeMessageFromResourceTypeValue(resourceTypeValue);
+        } else {
+          this._typeMessage = this.getTypeMessageFromResourceTypeValue(renderType.name);
+        }
+      } else {
+        this._typeMessage = this.getTypeMessageFromResourceTypeValue(renderType);
+      }
+    }
+  }
+
+  private getTypeMessageFromResourceTypeValue(resourceTypeValue: string) {
+    resourceTypeValue = resourceTypeValue.toLowerCase();
+
+    if (resourceTypeValue === "item") {
+      return this._object.firstMetadataValue("dc.type");
+    }
+
+    return `${resourceTypeValue}.listelement.badge`;
+  }
+
+  get object(): DSpaceObject {
+    return this._object;
+  }
+
+  get typeMessage(): string {
+    return this._typeMessage;
+  }
 }
