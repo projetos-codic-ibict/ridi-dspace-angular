3dbe9142ee5a9b8bd288b7f7a262ba7a97e46702
Gabriel Marcos <gabrielmarcosbguimaraes@gmail.com>
2025-11-09 13:13:35 -0300

    adicionando texto de apresentação do ridi

--- src/themes/ridi/app/presentation-page/presentation-page.component.ts
+++ src/themes/ridi/app/presentation-page/presentation-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'presentation-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './presentation-page.component.html',
   styleUrls: ['./presentation-page.component.scss']
 })
