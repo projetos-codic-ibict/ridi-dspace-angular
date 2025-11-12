17c3b0a5d57c3b5330440a248b1665be5fec8a79
Gabriel Marcos <gabrielmarcosbguimaraes@gmail.com>
2025-11-12 10:35:29 -0300

    montando a estrutura da página de política insitucional e suas chaves de tradução

--- src/themes/ridi/app/repository-policy/repository-policy.component.ts
+++ src/themes/ridi/app/repository-policy/repository-policy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'repository-policy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './repository-policy.component.html',
   styleUrls: ['./repository-policy.component.scss']
 })
