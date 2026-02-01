e91aefbb404ed32f054c57d7745ab55878ed7765
Gabriel Marcos <gabrielmarcosbguimaraes@gmail.com>
2026-02-01 16:22:45 -0300

    adicionando tela de contatos, manual de deposito e politica institucional

--- src/themes/ridi/app/contacts/contacts.component.ts
+++ src/themes/ridi/app/contacts/contacts.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'contacts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
